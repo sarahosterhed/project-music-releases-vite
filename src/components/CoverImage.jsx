e24ce69b27ba0369b119da9da5acd81ff619a7c8
joona-3 <52347755+joona-3@users.noreply.github.com>
2023-10-19 15:26:41 +0200

    fix icons import

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -1,11 +1,15 @@
+import heartIcon from "../assets/icons/heart.svg"
+import playIcon from "../assets/icons/play.svg"
+import dotsIcon from "../assets/icons/dots.svg"
+
 export const CoverImage = ({ coverImage, coverALT }) => {
     return (
         <div className="album-cover">
             <img className="cover-image" src={coverImage} alt={coverALT}></img>
             <div className="icons">
-                <img className="heart icon" src="/src/assets/icons/heart.svg"></img>
-                <img className="play icon" src="/src/assets/icons/play.svg"></img>
-                <img className="dots icon" src="/src/assets/icons/dots.svg"></img>
+                <img className="heart icon" src={heartIcon}></img>
+                <img className="play icon" src={playIcon}></img>
+                <img className="dots icon" src={dotsIcon}></img>
             </div>
         </div>
     )
