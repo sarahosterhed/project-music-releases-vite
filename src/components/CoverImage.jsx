c752bff47eae61435fecd22b0653abb4ac2b63b2
Sarah <sarah.osterhed@gmail.com>
2023-10-19 13:05:49 +0200

    Styling of Header and hover-items

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -1,6 +1,13 @@
 export const CoverImage = ({ coverImage, coverALT }) => {
     return (
-        <img className="cover-image" src={coverImage} alt={coverALT}></img>
+        <div className="album-cover">
+            <img className="cover-image" src={coverImage} alt={coverALT}></img>
+            <div className="icons">
+                <img className="heart icon" src="/src/assets/icons/heart.svg"></img>
+                <img className="play icon" src="/src/assets/icons/play.svg"></img>
+                <img className="dots icon" src="/src/assets/icons/dots.svg"></img>
+            </div>
+        </div>
     )
 
 }
