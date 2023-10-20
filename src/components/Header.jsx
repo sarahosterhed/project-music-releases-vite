e1f83dc424d69d5170b76679da6e23cc3f086d21
joona-3 <52347755+joona-3@users.noreply.github.com>
2023-10-20 10:00:54 +0200

    add css header + album

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ export const Header = () => {
     return (
         <header>
             <h1 className="header">New Albums & Singles</h1>
-            <p className="header">Technigo Education Team</p>
         </header>
     )
 }
